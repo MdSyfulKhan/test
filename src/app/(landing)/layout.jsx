3552fcf09e534cc08b7cdbc9e92b71d059bbdcba
MdSyfulKhan <mdsyfulkhan9899@gmail.com>
2024-03-09 12:45:10 +0600

    delete creative portfolio

--- src/app/(landing)/layout.jsx
+++ src/app/(landing)/layout.jsx
@@ -4,13 +4,10 @@ import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import React, { useEffect } from 'react'
 
-export default function Layout({ headerVariant , children}) {
-  // useEffect(() => {
-  //   window.scrollTo(0, 0);
-  // }, []);
+export default function Layout({children}) {
   return (
     <>
-      <Header variant={headerVariant} />
+      <Header />
       <CustomCursor />
       {children}
       <Footer />
--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,7 +1,23 @@
+'use client'
+import Div from "@/components/Div";
+import SectionHeading from "@/components/SectionHeading";
 
 
-export default function notFound() {
+export default function ErrorPage() {
   return (
-    <div>not-found</div>
-  )
+    <Div
+      className="cs-page_heading cs-style1 cs-center text-center cs-bg cs-error_page"
+      style={{ backgroundImage: 'url("/images/about_hero_bg.jpeg")' }}
+    >
+      <Div className="container">
+        <SectionHeading
+          title="This page could <br/>not be found."
+          subtitle="404 Errro"
+          btnText="Back To Home"
+          btnLink="/"
+          variant="cs-style1 text-center"
+        />
+      </Div>
+    </Div>
+  );
 }
